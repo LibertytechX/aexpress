315bff455864580c018d7418796bdef94c2b3733
dtekluva <kboysreel@gmail.com>
2026-02-15 12:15:48 +0100

    UPDATE[INSTANT VALIDATION OF PASSWORD]

--- frontend/MerchantPortal.jsx
+++ frontend/MerchantPortal.jsx
@@ -903,7 +903,7 @@ function SignupScreen({ onBack, onComplete }) {
             <div style={{ marginBottom: 16 }}>
               <label style={{ display: "block", fontSize: 13, fontWeight: 600, color: "#334155", marginBottom: 6 }}>Confirm Password</label>
               <input
-                type="password"
+                type="password" 
                 value={confirmPassword}
                 onChange={e => setConfirmPassword(e.target.value)}
                 placeholder="Re-enter password"
