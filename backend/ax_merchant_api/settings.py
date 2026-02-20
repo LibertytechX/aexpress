da063f445bcbf85b762d968313d506afb6a67f7b
Ayobami6 <ayobamidele006@gmail.com>
2026-02-20 10:31:47 +0100

    feat: Add `.ngrok-free.app` to default `ALLOWED_HOSTS`.

--- backend/ax_merchant_api/settings.py
+++ backend/ax_merchant_api/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fallback-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,.ngrok-free.app").split(
+    ","
+)
 
 
 # Application definition
